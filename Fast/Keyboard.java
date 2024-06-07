ddf4a6d1a04cf11d902c080ea2031a0968e91acf
Dodorex <dodorex@protonmail.com>
2024-06-07 22:14:13 +0200

    fix for keyboard

--- Fast/Keyboard.java
+++ Fast/Keyboard.java
@@ -15,20 +15,24 @@ public class Keyboard extends JFrame implements KeyListener
     }
     public void keyPressed(KeyEvent e)
     {
-        if(keysPressed.contains(e.getKeyChar()) == false)
+        if(keysPressed.contains(e.getKeyText(e.getKeyCode()).toLowerCase()) == false)
         {
-            keysPressed.add(e.getKeyText(e.getKeyCode()));
+            keysPressed.add(e.getKeyText(e.getKeyCode()).toLowerCase());
         }
     }
     public void keyReleased(KeyEvent e)
     {
-        if(keysPressed.contains(e.getKeyChar()) == true)
+        if(keysPressed.contains(e.getKeyText(e.getKeyCode()).toLowerCase()) == true)
         {
-            keysPressed.remove(e.getKeyText(e.getKeyCode()));
+            keysPressed.remove(e.getKeyText(e.getKeyCode()).toLowerCase());
         }
     }
     public boolean isKeyPressed(String key)
     {
-        return keysPressed.contains(key);
+        return keysPressed.contains(key.toLowerCase());
+    }
+    public ArrayList<String> getKeysPressed()
+    {
+        return keysPressed;
     }
 }
